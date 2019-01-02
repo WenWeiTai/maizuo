c4e26c5c8255f7b7ad64a7ef74292c329d5c3293
rxd8080 <578401154@qq.com>
2019-01-02 17:46:57 +0800

    城市静态页面

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-
+import { HashRouter as Router, Route } from 'react-router-dom'
+import City from './views/City'
 import './styles/base.less';
 
 class App extends React.Component {
-  constructor () {
-    super ()
-  }
-
   render () {
     return (
-      <div>主页</div>
+      <Router>
+        <Route path="/city" component={City}></Route>
+      </Router>
     )
   }
 }
