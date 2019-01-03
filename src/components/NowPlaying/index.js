05c798370bac0dce7a704e63e4dda25b6316afee
liuliuliu12345 <752309541@qq.com>
2019-01-03 18:12:24 +0800

    即将上映页面切换

---
+++ src/components/NowPlaying/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.less';
+class NowPlaying extends React.Component{
+  render() {
+    return (
+      <div className="lyj-cityList">
+      {
+        this.props.films.map((item,index) => {
+            return (
+              <Link to='/front?'>
+              <div className='ljy-List' key={index}>
+              <img src={require('@/images/img1.jpg')} alt="" />
+              <div className='lyj-filmsDetails'>
+                <div className='lyj-detailsList'>
+                <div className="lyj-title">
+                  <h2>{item.nm}</h2>
+
+                  <div className="lyj-3dBox">
+                      <div className="lyj3d">{`${item.showst}D`}</div>
+                      <div className="lyjIMAX">IMAX</div>
+                  </div>
+                </div>
+
+                <p className="p1">观众评<span>{item.sc}</span></p>
+                <p className="p2">{`主演 : ${item.star}`}</p>
+                <p className="p3">{item.showInfo}</p>
+
+                </div>
+
+                <div className='lyj-gouPiao'>
+                  <div className="gouPiao" onClick={this.onChange}>购票</div>
+                </div>
+              </div>
+
+            </div>
+            </Link>
+             )
+        })
+      }
+  </div>
+
+    )
+  }
+}
+export default NowPlaying;
