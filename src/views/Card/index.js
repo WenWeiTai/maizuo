89772f07f787ce7458be49b8e4c570dde9dd6a09
WenWeiTai <375354018@qq.com>
2019-01-03 10:35:17 +0800

    购物车静态页面完成

--- src/views/Card/index.js
+++ src/views/Card/index.js
@@ -26,6 +26,15 @@ export default class Card extends Component {
                   <i class="add-film">+</i>
                 </span>
               </li>
+              <li>
+                <span class="filmname">海王</span>
+                <span class="price">￥<i>100</i></span>
+                <span class="change-film-num">
+                  <i class="reduce-film">-</i>
+                  <span class="film-num">1</span>
+                  <i class="add-film">+</i>
+                </span>
+              </li>
             </ul>
             <div class="buy">
               <p>合计：<i>0</i>元</p>
