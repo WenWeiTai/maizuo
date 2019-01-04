f940b2fa2f795178fa29c2c91d96dcaeab2eafe6
liuliuliu12345 <752309541@qq.com>
2019-01-04 15:38:30 +0800

    下拉加载，点击购票跳转

--- src/views/Home/HomeFilms/index.js
+++ src/views/Home/HomeFilms/index.js
@@ -12,12 +12,11 @@ class LyjHome extends React.Component{
   constructor (props) {
     super (props)
     this.state = {
-      pageNum: 1, //当前页码
-      pageSize: 5, //当前页数
-      totalPage: 0, //总页数
-      films:[],
-      tatal: 0,
-      isShow:false,
+      pageNum: 1,    //当前页码
+      pageSize: 5,   //当前页数
+      totalPage: 0,  //总页数
+      films:[],      //电影数据
+
       loadMoreText: "点击加载...",
       tabs:[
         { title: '正在上映' },
@@ -32,29 +31,67 @@ class LyjHome extends React.Component{
         curCity: store.getState().city.curCity
       })
     })
+    this.loadMore = this.loadMore.bind(this);
   }
   // 组件销毁取消监听
   componentWillUnmount () {
     this.unsubscribe()
   }
 
-  // 组件挂载完成
+  // 组件挂载完成，调用ajax请求
   componentDidMount() {
-    axios.get('http://10.36.140.90:4000/api/film/list',{
-    // axios.get('http://192.168.1.113:4000/api/film/list',{
-      params: {
-        // 第二个参数是get请求的参数
-        pageNum: this.state.pageNum,
-        pageSize: this.state.pageSize
-      }
+    this.getFilmName();
+  }
 
-    }).then(res=>{
-      this.setState({
-        films: res.data.data.film
-      })
-  })
+
+  getFilmName() {
+    axios.get('http://10.36.140.90:4000/api/film/list',{
+   // axios.get('http://192.168.1.113:4000/api/film/list',{
+     params: {
+       pageNum: this.state.pageNum,
+       pageSize: this.state.pageSize
+     }
+
+   }).then(res =>{
+     let result = res.data;
+
+     this.setState(() => ({
+      // Math.ceil:想上取整(数据库的总页数/当前页数)
+       totalPage: Math.ceil(result.data.tatal / this.state.pageSize)
+     }))
+
+     if(this.state.pageNum >= this.state.totalPage) {
+          this.setState(() => ({
+            loadMoreText: this.loadMoreText = '别拉了，我是有底线的...'
+          }))
+     }
+     if(result.code === 0 ) {
+
+          this.setState(() => ({
+            // 原数组保存，和追加(concat：数组方法)
+            films: this.state.films.concat(result.data.film)
+          }))
+
+    } else {
+     alert(result.msg);
+   }
+
+ })
+}
+
+
+  // 点击加载更多
+  loadMore() {
+    // 当前页码小于总页数,就对当前页码加 1
+    if(this.state.pageNum < this.state.totalPage){
+        this.state.pageNum++;
+        this.getFilmName();//点击加载更多，调用ajax请求
+
+    }
+
 
   }
+
   render() {
     return (
       <Fragment>
@@ -90,7 +127,8 @@ class LyjHome extends React.Component{
                 <Tabs tabs={this.state.tabs} initialPage={0} animated={false} useOnPan={false}>
 
                   <div style={{ alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
-                      <NowPlaying films={this.state.films}></NowPlaying>
+                     {/* 传递组件的props 过去 */}
+                      <NowPlaying {...this.props} films={this.state.films}></NowPlaying>
                   </div>
                   <div style={{ alignItems: 'center', justifyContent: 'center',  backgroundColor: '#fff' }}>
                       <SoonPlaying films={this.state.films}></SoonPlaying>
@@ -98,7 +136,7 @@ class LyjHome extends React.Component{
                 </Tabs>
                 <WhiteSpace />
               </div>
-            <p className="p4">{this.state.loadMoreText}</p>
+            <p className="p4" onClick={this.loadMore}>{this.state.loadMoreText}</p>
         </div>
       </Fragment>
     )
