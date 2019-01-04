5efd4d7bd9c628a905c5e44c494ca25fb29fc9e7
zhangkun95 <502374689@qq.com>
2019-01-04 10:54:45 +0800

    zk详情页面信息功能补充

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import './index.less';
 
-import { NavBar, Icon} from 'antd-mobile';
+import { NavBar} from 'antd-mobile';
 // import { StickyContainer, Sticky } from 'react-sticky';
 import Usera from './Usera/inde';
 export default class User extends Component {
+  goback=()=>{
+    this.props.history.goBack()
+  }
   render() {
     return (
       <div>
         <NavBar
           mode="light"
-          icon={<Icon type="left" />}
+          // icon={<Icon type="left" />}
+          leftContent={<i  className="iconfont iconfont-jiantouzuo" onClick={this.goback.bind(this)} ></i>}
           onLeftClick={() => console.log('onLeftClick')}
           // rightContent={[
           //   <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
