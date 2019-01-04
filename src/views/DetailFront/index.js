fae5cde8e6d0000b89bfb9012d7d3a1a045d31f3
616151 <616151509@qq.com>
2019-01-04 15:09:57 +0800

    ss交互

--- src/views/DetailFront/index.js
+++ src/views/DetailFront/index.js
@@ -3,7 +3,7 @@ import './index.less';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import CinemaList from '@/components/CinemaList/index.js'
-import http from '../../rxdserver.js';
+// import http from '../../rxdserver.js';
 
 class front extends Component {
     constructor(props) {
@@ -12,7 +12,8 @@ class front extends Component {
             newdata: {},
             date: [],
             change: 0,
-            daTou: ''
+            id: '',
+            bool: true
         }
     }
     handleChange(i) {
@@ -21,25 +22,33 @@ class front extends Component {
             change: i.index
         })
     }
+    hideImg() {
+        console.log(this.refs);
+        // this.setState({
+        //     bool: !this.state.bool
+        // })
+        // if(this.state.bool === true) {
+        //     this.refs.true.style.display="none"
+        // } else {
+        //     this.refs.true.style.display=""
+        // }
+
+    }
     componentDidMount() {
-        axios.get('./ss-detail/phone.json').then((res) => {
-            this.setState({
-                newdata: res.data.detailMovie
-            })
-        })
         axios.get('./ss-detail/cinema.json').then((res) => {
             this.setState({
                 date: res.data.showDays.dates
             })
         })
-        http.get('http://10.36.140.90:4000/api/filmid/id', {
+        const newId = this.props.location.search.slice(1)
+        axios.get('http://10.36.140.90:4000/api/filmid/id',{
             params: {
-              id:1206875
+              id: newId
             }
           }).then((res) => {
-            console.log(res)
             this.setState({
-                daTou: res
+                newdata: res.data.data[0],
+                id: newId
             })
         })
     }
@@ -56,25 +65,25 @@ class front extends Component {
             <p className="nav-headerr">{this.state.newdata.nm}</p>
             </header>
         {/* detail */}
-        <Link to="/detail">
-            <div className="detail box-flex">
+        <Link to={`/detail?${this.state.id}`}>
+            <div className="detail box-flex" ref>
             <div className="poster">
-                <img alt="" src="//p1.meituan.net/148.208/movie/a596474c1c29118d908d1eff0fd4293f1017066.jpg" />
+                <img alt="" src={this.state.newdata.img} />
             </div>
             <div className="content flex">
                 <div className="title middle line-ellipsis">{this.state.newdata.nm}</div>
-                <div className="title-en-name line-ellipsis">{this.state.newdata.enm}</div>
+                <div className="title-en-name line-ellipsis">SS weilian</div>
                 <div className="score line-ellipsis">
                 <span>{this.state.newdata.sc}</span>
                 </div>
                 <div className="type line-ellipsis">
-                <span>{this.state.newdata.cat}</span>
+                <span>剧情 喜剧</span>
                 <div className="type-group">
                     <img alt="" className="sd-imax" src=""/>
                 </div>
                 </div>
-                <div className="src line-ellipsis">{this.state.newdata.src}/{this.state.newdata.dur}分钟</div>
-                <div className="pubDesc line-ellipsis">{this.state.newdata.pubDesc}</div>
+                <div className="src line-ellipsis">中国大陆/103分钟</div>
+                <div className="pubDesc line-ellipsis">{this.state.newdata.rt}上映</div>
             </div>
             <div className="arrow-g">
                 <img alt="" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAWCAYAAAAfD8YZAAAAAXNSR0IArs4c6QAAAS5JREFUOBGVkktuwkAMhpOoJ+hhSouE2HbDgiVCNCAOVtQHnINHhbgMJ6g6/f9gR848kmDJeOzxZ3scskzEOfcKXUMLjXXZByYQhJlqMvz3PM//1E9Z7fJoEp5wXvWZoILRZQtg7xVYdhXQzuR2XoEB/NYCOSkVdKI/g441BnuBbmI7aMAE7ilgxyaboYOD4RMO9EWiTwhgJksBLtEvUNolRmGvwJG+yDNsXSB4s2aplR3M4Y80BnuGfiQ7m0Q2qP6JJvaLc/VpTKx5lPe9IfpibviMb+4lOXYXyGLRsRPgSTsSpASdW8Av+YQ3Er+NzgIuELdvZMcAZIUaBsgpCA55IZIEeV+NLSC32hskXCTAH9xFRyWkUsgSrhqAJfjpL8fch0dMMIGWMkmYEIn8Az5Wgp5LHlhmAAAAAElFTkSuQmCC"/>
@@ -96,7 +105,7 @@ class front extends Component {
             </ul>
             </div>
         {/* nav-wrap */}
-            <div className="nav-wrap filter-nav-wrap">
+            <div className="nav-wrap filter-nav-wrap" onClick = {this.hideImg.bind(this)}>
             <div className="tab mb-line-b">
                     <div className="item" data-tab=".region">全城<span className="drop"></span></div>
                     <div className="item" data-tab=".brand">品牌<span className="drop"></span></div>
